2bbed7376f09b02351bc87db0704c16d3d6728ee
Hovo33651 <hovogevorgyan94@mail.ru>
2022-03-14 14:36:46 +0400

    SpringItemAm

--- src/main/java/com/example/itemam/service/UserImageService.java
+++
@@ -1,9 +0,0 @@
-package com.example.itemam.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserImageService {
-}
